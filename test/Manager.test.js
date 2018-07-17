98bf46934ca3d23bc4dc7f2e872a0de679640b2f
Luis Osta <luisosta1999@gmail.com>
2018-07-17 17:17:15 -0500

    added testing packages, ran truffle  compile

---
+++ test/Manager.test.js
@@ -0,0 +1,251 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+
+const web3 = new Web3(ganache.provider())
+
+const compiledToken = require('../build/contracts/RotoToken.json')
+const compiledManager = require('../build/contracts/RotoManager.json')
+
+let accounts
+
+let manager
+let token
+let address
+
+// general listeners
+let TournamentListener
+let TokenListener
+
+// staking related listeners
+let StakeProcessedListener
+let StakeReleasedListener
+let SubmissionRewardedListener
+let StakeDestroyedListener
+
+// tournament info
+let tournamentID
+let etherPrize
+let rotoPrize
+
+describe('RotoManager Contract', async () => {
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+
+    token = await new web3.eth.Contract(compiledToken.abi)
+      .deploy({ data: compiledToken.bytecode })
+      .send({ from: accounts[0], gas: 1500000 })
+
+    manager = await new web3.eth.Contract(compiledManager.abi)
+      .deploy({ data: compiledManager.bytecode })
+      .send({ from: accounts[0], gas: 1500000 })
+
+    await manager.methods
+      .setTokenContract(token.options.address)
+      .send({ from: accounts[0] })
+    address = manager.options.address
+
+    await token.methods
+      .setManagerContract(manager.options.address)
+      .send({ from: accounts[0] })
+
+    // Tournament Listener
+    TournamentListener = await manager.events.TournamentCreated({
+      fromBlock: 0
+    })
+
+    // Token Changed
+    TokenListener = await manager.events.TokenChanged({ fromBlock: 0 })
+
+    // Stake Processed
+    StakeProcessedListener = await manager.events.StakeProcessed({
+      fromBlock: 0
+    })
+
+    // Stake Released
+    StakeReleasedListener = await manager.events.StakeReleased({ fromBlock: 0 })
+
+    //Submission Rewarded
+    SubmissionRewardedListener = await manager.events.SubmissionRewarded({ fromBlock: 0 })
+
+    // Stake Destroyed
+    StakeDestroyedListener = await manager.events.StakeDestroyed({
+      fromBlock: 0
+    })
+
+    //Creates a Tournament
+    tournamentID = web3.utils.randomHex(32)
+    etherPrize = 12
+    rotoPrize = await web3.utils.toWei('5000000')
+    await manager.methods.createTournament(tournamentID, etherPrize, rotoPrize)
+      .send({ from: accounts[0], gas: 1500000, value: await web3.utils.toWei('10') })
+
+    //transfer 100 ROTO to account 1
+    await token.methods.transferFromContract(accounts[1], await web3.utils.toWei('100')).send({
+      from: accounts[0]
+    })
+  })
+
+  it('should have deployed the contracts propertly', async () => {
+    assert.ok(address)
+    assert.ok(token.options.address);
+  })
+
+  it('should have 10 ether in balance', async() => {
+    let balance = await web3.utils.fromWei(await web3.eth.getBalance(address));
+
+    assert.equal('10', balance)
+  })
+
+  it('should have created a tournament', async() => {
+      TournamentListener.on('data', event => {
+        let returnValues = event.returnValues
+
+        assert.equal(tournamentID, returnValues.tournamentID);
+        assert.equal(etherPrize, returnValues.etherPrize)
+        assert.equal(rotoPrize, returnvalues.rotoPrize)
+      })
+  })
+  it('shoudl give account 1 100 ROTO', async() => {
+      let balance = await token.methods.balanceOf(accounts[1]).call()
+      balance = await web3.utils.fromWei(balance)
+
+      assert.equal('100', balance)
+  })
+  it('should allow a user to stake ROTO on a tournament', async() => {
+
+      let stake = await web3.utils.toWei('10')
+      let staker = accounts[1]
+
+      let initial_balance = await token.methods.balanceOf(staker).call()
+
+      StakeProcessedListener.on('data', event => {
+          let returnValues = event.returnValues
+
+          assert.equal(tournamentID, returnValues.tournamentID);
+          assert.equal(staker, returnValues.staker)
+          assert.equal(stake, returnValues.totalAmountStaked)
+      })
+
+      await manager.methods.stake(stake, tournamentID).send({ from: staker })
+      let final_balance = await token.methods.balanceOf(staker).call()
+      
+      let expected_value = Number(initial_balance) - Number(stake)
+      assert.equal(expected_value.toString(), final_balance)
+  })
+
+  it('should allow the owner to release staked ROTO, and give it 0 ether', async() => {
+    let stake = await web3.utils.toWei('10')
+    let staker = accounts[1]
+    let etherReward = 0
+
+    //gets the initial ROTO balance
+    let initial_balance = await token.methods.balanceOf(staker).call()
+    
+    //stakes 10 ROTO in the tournament
+    await manager.methods.stake(stake, tournamentID).send({ from: staker })
+    
+    StakeReleasedListener.on('data', event => {
+      let returnValues = event.returnValues
+
+      assert.equal(tournamentID, returnValues.tournamentID)
+      assert.equal(staker, returnValues.stakerAddress)
+      assert.equal(etherReward, returnValues.etherReward)
+      assert.equal(stake, returnValues.rotoStaked)
+    })
+
+    
+    let result = await manager.methods.releaseRoto(staker, tournamentID, etherReward).send({ from: accounts[0] })
+    console.log('ReleaseRoto Gas Used: ', result.gasUsed)
+    let final_balance = await token.methods.balanceOf(staker).call()
+    
+    assert.equal(initial_balance, final_balance)
+  })
+
+  it('should allow the owner to release staked ROTO, and give it 2 ether', async() => {
+    let stake = await web3.utils.toWei('10')
+    let staker = accounts[1]
+    let etherReward = await web3.utils.toWei('2')
+
+    //Gets initial ether and roto balance
+    let initial_ether_balance = await web3.eth.getBalance(staker);
+    let initial_roto_balance = await token.methods.balanceOf(staker).call()
+
+    //stakes 10 ROTO in the tournament
+    await manager.methods.stake(stake, tournamentID).send({ from: staker })
+
+    StakeReleasedListener.on('data', event => {
+      let returnValues = event.returnValues
+
+      assert.equal(tournamentID, returnValues.tournamentID)
+      assert.equal(staker, returnValues.stakerAddress)
+      assert.equal(etherReward, returnValues.etherReward)
+      assert.equal(stake, returnValues.rotoStaked)
+    })
+
+    let result = await manager.methods.releaseRoto(staker, tournamentID, etherReward).send({ from: accounts[0] })
+    console.log('ReleaseRoto Gas Used: ',result.gasUsed)
+
+    //gets final ether and roto balances
+    let final_ether_balance = await web3.eth.getBalance(staker)
+    let final_roto_balance = await token.methods.balanceOf(staker).call()
+
+    let expected_ether_value = Number(initial_ether_balance)
+    let end_ether_value = Number(final_ether_balance)
+
+    assert.equal(final_roto_balance, initial_roto_balance);
+    assert(end_ether_value > expected_ether_value);
+  })
+
+  it('should allow the owner to destroy staked ROTO', async() => {
+    let stake = await web3.utils.toWei('10')
+    let staker = accounts[1]
+
+    //gets the initial ROTO balance
+    let initial_balance = await token.methods.balanceOf(staker).call()
+
+    //stakes 10 ROTO in the tournament
+    await manager.methods.stake(stake, tournamentID).send({ from: staker })
+
+    StakeDestroyedListener.on('data', event => {
+      let returnValues = event.returnValues
+      
+      assert.equal(tournamentID, returnValues.tournamentID)
+      assert.equal(staker, returnValues.stakerAddress)
+      assert.equal(stake, returnValues.rotoLost)
+    })
+
+    let result = await manager.methods.destroyRoto(staker, tournamentID).send({ from: accounts[0] })
+    console.log('DestroyRoto Gas Used: ',result.gasUsed)
+
+    let final_balance = await token.methods.balanceOf(staker).call()
+    let expected = initial_balance - stake;
+
+    assert.equal(final_balance, expected);
+  })
+
+  it('should allow the owner to reward unstaked submissions', async() => {
+      let user = accounts[1]
+      let rotoPrize = await web3.utils.toWei('5')
+      
+      //gets the initial ROTO balance
+      let initial_balance = await token.methods.balanceOf(user).call()
+
+      SubmissionRewardedListener.on('data', event => {
+        let returnValues = event.returnValues
+        
+        assert.equal(tournamentID, returnValues.tournamentID)
+        assert.equal(user, returnValues.stakerAddress)
+        assert.equal(rotoPrize, returnValues.rotoReward)
+      })
+
+      let result = await manager.methods.rewardRoto(user, tournamentID, rotoPrize).send({ from: accounts[0] })
+      console.log('RewardRoto Gas Used: ', result.gasUsed)
+
+      let final_balance = await token.methods.balanceOf(user).call()
+      let expected = Number(initial_balance) + Number(rotoPrize)
+      
+      assert.equal(final_balance, expected)
+  })
+})
---
+++ test/Token.test.js
@@ -0,0 +1,199 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+
+const web3 = new Web3(ganache.provider())
+
+const compiledToken = require('../build/contracts/RotoToken.json')
+let accounts
+
+let token
+let address
+let manager
+let supply
+let decimal
+
+describe('RotoToken Contract', async () => {
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+    token = await new web3.eth.Contract(compiledToken.abi)
+      .deploy({ data: compiledToken.bytecode })
+      .send({ from: accounts[0], gas: 1500000 })
+
+    await token.methods.setManagerContract(accounts[3]).send({
+      from: accounts[0]
+    })
+
+    manager = accounts[3]
+
+    supply = await token.methods.totalSupply().call()
+    decimal = await token.methods.decimals().call()
+    address = token.options.address
+  })
+  it('should deploy the contract correctly', () => {
+    assert.ok(address)
+  })
+
+  it('should set the total supply to 21 million', () => {
+    const expected = 21000000
+    assert.equal(expected, supply / 10 ** decimal)
+  })
+
+  it('should set the decimals to 18', async () => {
+    const expected = 18
+    assert.equal(expected, decimal)
+  })
+
+  it('should send all of the initial amount to the contract', async () => {
+    let contract_balance = await token.methods
+      .balanceOf(address)
+      .call({ from: accounts[0] })
+    assert.equal(supply, contract_balance)
+  })
+  it('should set the initial balance of account 1 and 2 to 0', async () => {
+    const account_balance = await token.methods.balanceOf(accounts[1]).call()
+    const account1_balance = await token.methods.balanceOf(accounts[1]).call()
+
+    assert.equal(0, account1_balance)
+    assert.equal(account1_balance, account_balance)
+  })
+
+  it('should set the manager contract correctly', async () => {
+    const result = await token.methods.getManager().call()
+
+    assert.equal(manager, result)
+  })
+  it('should allow the owner to transfer ROTO from the contract', async () => {
+    let withdrawal = '1000000000000000000000000'
+    const result = await token.methods
+      .transferFromContract(accounts[0], withdrawal)
+      .send({ from: accounts[0] })
+    assert(result)
+
+    let account1_balance = await token.methods.balanceOf(accounts[0]).call()
+    assert.equal(1000000, Number(account1_balance) / 10 ** decimal)
+  })
+
+  it('should transfer 500k ROTO from account 0 to account 1', async () => {
+    let withdrawal = '1000000000000000000000000'
+    const withdraw_result = await token.methods
+      .transferFromContract(accounts[0], withdrawal)
+      .send({ from: accounts[0] })
+    assert(withdraw_result)
+
+    let account_balance = await token.methods.balanceOf(accounts[0]).call()
+    assert.equal(1000000, Number(account_balance) / 10 ** decimal)
+
+    let transferAmount = '500000000000000000000000'
+    const transfer_result = await token.methods
+      .transfer(accounts[1], transferAmount)
+      .send({ from: accounts[0] })
+    assert(transfer_result)
+
+    let account1_balance = await token.methods.balanceOf(accounts[1]).call()
+    assert.equal(500000, Number(account1_balance) / 10 ** decimal)
+  })
+
+  it('should set the base allowance for a given address to 0', async () => {
+    let allowance = await token.methods
+      .allowance(accounts[0], accounts[1])
+      .call()
+    assert.equal(0, allowance)
+  })
+
+  it('should allow the manager to check whether a user can stake ROTO', async () => {
+    let withdrawal = '10000000000000000000000'
+    const withdraw_result = await token.methods
+      .transferFromContract(accounts[1], withdrawal)
+      .send({ from: accounts[0] })
+
+    let stake = '10000000000000000000000'
+    let staker = accounts[1]
+
+    let result = await token.methods
+      .canStake(staker, stake)
+      .send({ from: manager })
+    assert(result)
+  })
+
+  it('should allow the manager to send a request to stake ROTO', async () => {
+    let withdrawal = '1000000000000000000000000'
+    const withdraw_result = await token.methods
+      .transferFromContract(accounts[1], withdrawal)
+      .send({ from: accounts[0] })
+
+    let stake = '1000000000000000000000'
+    let staker = accounts[1]
+    let initial_balance = await token.methods.balanceOf(staker).call()
+
+    let result = await token.methods
+      .stakeRoto(staker, stake)
+      .send({ from: manager })
+    let final_balance = await token.methods.balanceOf(staker).call()
+
+    assert.equal(Number(initial_balance) - Number(stake), final_balance)
+    assert(result)
+  })
+
+  it('should allow the manager to release a staked ROTO', async () => {
+    let withdrawal = '1000000000000000000000000'
+    const withdraw_result = await token.methods
+      .transferFromContract(accounts[1], withdrawal)
+      .send({ from: accounts[0] })
+
+    let stake = '1000000000000000000000'
+    let staker = accounts[1]
+    let initial_balance = await token.methods.balanceOf(staker).call()
+
+    await token.methods.stakeRoto(staker, stake).send({ from: manager })
+
+    let result = await token.methods
+      .releaseRoto(staker, stake)
+      .send({ from: manager })
+    assert(result)
+
+    let final_balance = await token.methods.balanceOf(staker).call()
+    assert.equal(initial_balance, final_balance)
+  })
+
+  it('should allow the manager to destroy staked ROTO', async () => {
+    let withdrawal = '1000000000000000000000000'
+    await token.methods
+      .transferFromContract(accounts[1], withdrawal)
+      .send({ from: accounts[0] })
+
+    let initial_roto_balance = await token.methods.balanceOf(address).call()
+
+    let stake = '1000000000000000000000'
+    let staker = accounts[1]
+    let initial_balance = await token.methods.balanceOf(staker).call()
+
+    await token.methods.stakeRoto(staker, stake).send({ from: manager })
+
+    let result = await token.methods
+      .destroyRoto(staker, stake)
+      .send({ from: manager })
+    assert(result)
+
+    let final_balance = await token.methods.balanceOf(staker).call()
+    let final_roto_balance = await token.methods.balanceOf(address).call()
+
+    assert.equal(Number(initial_balance) - Number(stake), final_balance)
+
+    assert.equal('20001000000000000000000000', final_roto_balance)
+  })
+
+  it('should allow the manager to reward ROTO to non-staked submissions', async () => {
+    let user = accounts[1]
+    let reward = 10 * 10 ** 18
+    let initial_balance = await token.methods.balanceOf(user).call()
+
+    let result = await token.methods
+      .rewardRoto(user, reward)
+      .send({ from: manager })
+    assert.ok(result)
+
+    let final_balance = await token.methods.balanceOf(user).call()
+    assert.equal(Number(initial_balance) + reward, final_balance)
+  })
+})
